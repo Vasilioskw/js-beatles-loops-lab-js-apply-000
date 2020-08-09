e14bc910af3e906d941ea177b23e29cb22e890db
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 23:29:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const singers = ["John Lennon", "Ringo Star", "Paul McCartney", "Dizzy"];
 const instruments = ["guitar", "drums", "piano", "trumpet"];
 
+var theBeatlesPlay = function (musiciansArray, instrumentsArray) {
+	var collect = [];
+	for (var i = 0; i<musiciansArray.length; i++) {
+	collect.push(musiciansArray[i] + " plays " + instrumentsArray[i]);
+	}
+	return(collect);
     facts = [
       "He was the last Beatle to learn to drive",
       "He was never a vegetarian",
