5b0ac52fead7113283333753065a01542d3099ee
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 23:45:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,5 @@ theBeatlesPlay(singers, instruments);
 
 var facts = ["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"];
 
+johnLennonFacts(String)
+
