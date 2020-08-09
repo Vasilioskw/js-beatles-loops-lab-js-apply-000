d491afb53a67cce6ae78f80ccccf59b73dfcbfb8
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 23:19:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // add solution here
 function theBeatlesPlay(musicians)
 function theBeatlesPlay(instruments)
-var string = ["Saab", "Volvo", "BMW"];
-}
-    const facts = [
+
+    facts = [
       "He was the last Beatle to learn to drive",
       "He was never a vegetarian",
       "He was a choir boy and boy scout",
       "He hated the sound of his own voice"
-    ];
+    ]
+    
