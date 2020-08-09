f5a829070351f76a343c481bc00854b94585f830
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 23:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 const singers = ["John Lennon", "Ringo Star", "Paul McCartney", "Dizzy"];
 const instruments = ["guitar", "drums", "piano", "trumpet"];
 
-var theBeatlesPlay = function (singers, instruments) {
+var theBeatlesPlay = function (singersArray, instrumentsArray) {
 	var collect = [];
-	for (var i = 0; i<musiciansArray.length; i++) {
+	for (var i = 0; i<singers.length; i++) {
 	collect.push(musiciansArray[i] + " plays " + instrumentsArray[i]);
 	}
 	return(collect);
