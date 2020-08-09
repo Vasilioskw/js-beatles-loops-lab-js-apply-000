90227129998025122c5355ebce1a6c2760c4c727
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-09 23:30:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // add solution here
+
+
 const singers = ["John Lennon", "Ringo Star", "Paul McCartney", "Dizzy"];
 const instruments = ["guitar", "drums", "piano", "trumpet"];
 
@@ -8,10 +10,4 @@ var theBeatlesPlay = function (musiciansArray, instrumentsArray) {
 	collect.push(musiciansArray[i] + " plays " + instrumentsArray[i]);
 	}
 	return(collect);
-    facts = [
-      "He was the last Beatle to learn to drive",
-      "He was never a vegetarian",
-      "He was a choir boy and boy scout",
-      "He hated the sound of his own voice"
-    ]
-    
+};
